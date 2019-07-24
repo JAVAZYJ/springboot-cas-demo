d318a2949af59e3196ef8afa20778d35357a27c9
YiJieZhu <18112855600@163.com>
2019-07-24 23:31:02 +0800

    2

---
+++ cas-client/src/main/java/com/example/cas/client/CasClientApplication.java
@@ -0,0 +1,16 @@
+package com.example.cas.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YiJieZhu
+ */
+@SpringBootApplication
+public class CasClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasClientApplication.class, args);
+    }
+
+}
---
+++ cas-server/src/main/java/com/example/cas/server/CasServerApplication.java
@@ -0,0 +1,16 @@
+package com.example.cas.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YiJieZhu
+ */
+@SpringBootApplication
+public class CasServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasServerApplication.class, args);
+    }
+
+}
